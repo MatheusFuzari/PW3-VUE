1ba9d36c968e61054c0c4c39969d3c7f7afe69a6
MatheusFuzari <matheus.fuzari004@gmail.com>
2024-09-24 19:39:31 -0300

    first pipeline commit

--- aic.tf
+++ aic.tf
@@ -13,31 +13,31 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "fuzari-mangebot-terraform-group" {
-  name = "fuzari-mangebot-terraform-group"
+resource "azurerm_resource_group" "fuzari-mangebot-second-version-group" {
+  name = "fuzari-mangebot-second-version-group"
   location = "eastus2"
 }
 
-resource "azurerm_service_plan" "fuzari-mangebot-sp" {
-  name = "fuzari-mangebot-sp"
-  resource_group_name = azurerm_resource_group.fuzari-mangebot-terraform-group.name
-  location = azurerm_resource_group.fuzari-mangebot-terraform-group.location
+resource "azurerm_service_plan" "fuzari-mangebot-second-version-sp" {
+  name = "fuzari-mangebot-second-version-sp"
+  resource_group_name = azurerm_resource_group.fuzari-mangebot-second-version-group.name
+  location = azurerm_resource_group.fuzari-mangebot-second-version-group.location
   sku_name = "S1"
   os_type = "Windows"
 }
 
-resource "azurerm_windows_web_app" "fuzari-mangebot-app" {
-  name = "fuzari-mangebot-app"
-  resource_group_name = azurerm_resource_group.fuzari-mangebot-terraform-group.name
-  location = azurerm_resource_group.fuzari-mangebot-terraform-group.location
-  service_plan_id = azurerm_service_plan.fuzari-mangebot-sp.id
+resource "azurerm_windows_web_app" "fuzari-mangebot-second-version-app" {
+  name = "fuzari-mangebot-second-version-app"
+  resource_group_name = azurerm_resource_group.fuzari-mangebot-second-version-group.name
+  location = azurerm_resource_group.fuzari-mangebot-second-version-group.location
+  service_plan_id = azurerm_service_plan.fuzari-mangebot-second-version-sp.id
   site_config {
     always_on = false
   }
 }
 
-resource "azurerm_windows_web_app_slot" "fuzari-mangebot-terraform-app-slot-qa" {
-  name = "fuzari-mangebot-terraform-app-slot-qa"
-  app_service_id = azurerm_windows_web_app.fuzari-mangebot-app.id
+resource "azurerm_windows_web_app_slot" "fuzari-mangebot-second-version-app-slot-qa" {
+  name = "fuzari-mangebot-second-version-app-slot-qa"
+  app_service_id = azurerm_windows_web_app.fuzari-mangebot-second-version-app.id
   site_config { }
 }
